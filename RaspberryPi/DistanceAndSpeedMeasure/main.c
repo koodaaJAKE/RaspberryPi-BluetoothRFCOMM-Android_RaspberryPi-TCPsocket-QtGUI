b9b4b73adf25beb8b7879b7078eb7481a63efa97
koodaaJAKE <jarkko.heikkinen90@gmail.com>
2016-06-11 20:07:18 +0300

    :)

--- RaspberryPi/DistanceAndSpeedMeasure/main.c
+++ RaspberryPi/DistanceAndSpeedMeasure/main.c
@@ -12,12 +12,7 @@
 int main(void)
 {
 	/* Thread sensor data structure */
-	thread_data_t *sensorData;
-	sensorData = malloc(sizeof(thread_data_t));
-	if(sensorData == NULL) {
-		perror("malloc error: \n");
-		return -1;
-	}
+	thread_data_t sensorData;
 
 	/* Initialize the structure of MySQL constants */
 	mySqlConstants p_mySqlConstants = {
@@ -46,7 +41,7 @@ int main(void)
 	setType_LCD(4, 16);
 	setBacklight_LCD(250);
 
-	initializeMutex(sensorData);
+	initializeMutex(&sensorData);
 
 	initializeMySQL(&p_mySqlConstants);
 
@@ -54,42 +49,42 @@ int main(void)
 	/* Create Threads                                           */
 	/************************************************************/
 #ifdef TCPSOCKET
-	iret = pthread_create(&measureHRLVEZ0Thread, NULL, measureHRLVEZ0, (void*)sensorData);
+	iret = pthread_create(&measureHRLVEZ0Thread, NULL, measureHRLVEZ0, (void*)&sensorData);
 	if(iret)
 	{
 		fprintf(stderr, "Error - pthread_create() return code: %d\n", iret);
 		exit(EXIT_FAILURE);
 	}
 
-	iret1 = pthread_create(&TCP_SocketThread, NULL, TCP_Socket, (void*)sensorData);
+	iret1 = pthread_create(&TCP_SocketThread, NULL, TCP_Socket, (void*)&sensorData);
 	if(iret1)
 	{
 		fprintf(stderr, "Error - pthread_create() return code: %d\n", iret1);
 		exit(EXIT_FAILURE);
 	}
 
-	iret3 = pthread_create(&mySqlThread, NULL, mySQL, (void*)sensorData);
+	iret3 = pthread_create(&mySqlThread, NULL, mySQL, (void*)&sensorData);
 	if(iret3)
 	{
 		fprintf(stderr, "Error - pthread_create() return code: %d\n", iret3);
 		exit(EXIT_FAILURE);
 	}
 #else
-	iret = pthread_create(&measureHRLVEZ0Thread, NULL, measureHRLVEZ0, (void*)sensorData);
+	iret = pthread_create(&measureHRLVEZ0Thread, NULL, measureHRLVEZ0, (void*)&sensorData);
 	if(iret)
 	{
 		fprintf(stderr, "Error - pthread_create() return code: %d\n", iret);
 		exit(EXIT_FAILURE);
 	}
 
-	iret2 = pthread_create(&bluetoothRFCOMMThread, NULL, bluetoothRFCOMM, (void*)sensorData);
+	iret2 = pthread_create(&bluetoothRFCOMMThread, NULL, bluetoothRFCOMM, (void*)&sensorData);
 	if(iret2)
 	{
 		fprintf(stderr, "Error - pthread_create() return code: %d\n", iret2);
 		exit(EXIT_FAILURE);
 	}
 
-	iret3 = pthread_create(&mySqlThread, NULL, mySQL, (void*)sensorData);
+	iret3 = pthread_create(&mySqlThread, NULL, mySQL, (void*)&sensorData);
 	if(iret3)
 	{
 		fprintf(stderr, "Error - pthread_create() return code: %d\n", iret3);
@@ -108,7 +103,6 @@ int main(void)
 	pthread_join(mySqlThread, NULL);
 #endif
 	printf("Threads closed!\n");
-	free(sensorData);
 	clear_LCD();
 	setBacklight_LCD(0);
     serialLCD_Close();
